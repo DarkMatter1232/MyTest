e67e6b7d5610662e4eb801a1cc80451e176d6650
DarkMatter <miguel.r.r.lima22@gmail.com>
2025-03-13 19:11:48 -0300

    Delete DNSoptmizer.bat

--- DNSoptmizer.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-:: Abrir o CMD como administrador e executar os comandos dentro do CMD
-PowerShell -Command "Start-Process cmd -ArgumentList '/c ipconfig /flushdns && ipconfig /release && ipconfig /renew && exit' -Verb runAs"
